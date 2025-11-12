a7d58dfd6be35c42b9b1516510a5651cad22f6bc
Jumaannjii <135129916+Jumaannjii@users.noreply.github.com>
2025-11-12 19:51:26 +0700

    tutorial 8 selesai

--- lib/screens/news_entry_list.dart
+++ lib/screens/news_entry_list.dart
@@ -18,12 +18,12 @@ class _NewsEntryListPageState extends State<NewsEntryListPage> {
     // TODO: Replace the URL with your app's URL and don't forget to add a trailing slash (/)!
     // To connect Android emulator with Django on localhost, use URL http://10.0.2.2/
     // If you using chrome,  use URL http://localhost:8000
-    
-    final response = await request.get('http://[YOUR_APP_URL]/json/');
-    
+
+    final response = await request.get('http://localhost:8000/json/');
+
     // Decode response to json format
     var data = response;
-    
+
     // Convert json data to NewsEntry objects
     List<NewsEntry> listNews = [];
     for (var d in data) {
@@ -38,9 +38,7 @@ class _NewsEntryListPageState extends State<NewsEntryListPage> {
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('News Entry List'),
-      ),
+      appBar: AppBar(title: const Text('News Entry List')),
       drawer: const LeftDrawer(),
       body: FutureBuilder(
         future: fetchNews(request),
@@ -64,14 +62,14 @@ class _NewsEntryListPageState extends State<NewsEntryListPage> {
                 itemBuilder: (_, index) => NewsEntryCard(
                   news: snapshot.data![index],
                   onTap: () {
-                    // Show a snackbar when news card is clicked
-                    ScaffoldMessenger.of(context)
-                      ..hideCurrentSnackBar()
-                      ..showSnackBar(
-                        SnackBar(
-                          content: Text("You clicked on ${snapshot.data![index].title}"),
-                        ),
-                      );
+                    // Navigate to news detail page
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            NewsDetailPage(news: snapshot.data![index]),
+                      ),
+                    );
                   },
                 ),
               );
